4ced3b8aca74d2ed58d8ff042068b5cfd04b63b7
Patteco03 <thiagodark1995@gmail.com>
2019-03-26 15:42:16 -0500

    creator pages

--- js/script.js
+++ js/script.js
@@ -11,5 +11,13 @@ $(document).ready(function(){
   $('.cnpj').mask('00.000.000/0000-00', {reverse: true});
   $('.money').mask('000.000.000.000.000,00', {reverse: true});
   $('.money2').mask("#.##0,00", {reverse: true});
-  
+
+  $(document).on("keydown", ".cep", function () {
+      var num = ($(this).val().length);
+      if (num > 7) {
+        $('.loginin').css('display','block')
+      }else{
+        $('.loginin').css('display','none')
+      }
+  });
 });
