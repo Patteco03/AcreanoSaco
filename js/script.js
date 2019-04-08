7261ca40a486e53c9937fe30fd4d19d150d55ea9
Patteco03 <thiagodark1995@gmail.com>
2019-04-08 15:49:33 -0500

    chekout version finish

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $('.date_time').mask('00/00/0000 00:00:00');
   $('.cep').mask('00000-000');
   $('.phone').mask('0000-0000');
-  $('.phone_with_ddd').mask('(00) 0000-0000');
+  $('.phone_with_ddd').mask('(00) 00000-0000');
   $('.phone_us').mask('(000) 000-0000');
   $('.mixed').mask('AAA 000-S0S');
   $('.cpf').mask('000.000.000-00', {reverse: true});
@@ -13,11 +13,35 @@ $(document).ready(function(){
   $('.money2').mask("#.##0,00", {reverse: true});
 
   $(document).on("keydown", ".cep", function () {
-      var num = ($(this).val().length);
-      if (num > 7) {
-        $('.loginin').css('display','block')
-      }else{
-        $('.loginin').css('display','none')
-      }
+    var num = ($(this).val().length);
+    if (num > 7) {
+      $('.loginin').css('display','block')
+    }else{
+      $('.loginin').css('display','none')
+    }
+  });
+
+  var content = $('#content');
+  $('.list-menu a').on('click', function( e ){
+    e.preventDefault();
+    
+    var href = $( this ).attr('href');
+    $.ajax({
+      type: 'GET',
+      url: href,
+      dataType: 'text',
+      cache: false,
+      beforeSend: function () {
+        $('#reloade').css('display','block');
+      },
+      success: function( response ){
+        $('#reloade').css('display','none');
+         content.html(response);
+        },
+        error: function (erro) {
+          console.log(erro);
+        }
+      });
+
   });
 });
